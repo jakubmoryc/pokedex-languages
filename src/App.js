8471751ae50301d1ae056917a6fa9131cde34dc3
Mortzcent <moryckuba@gmail.com>
2019-08-23 18:07:18 +0200

    usable array finished, missing types

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 import Display from './components/Display/Display';
 
-import pokemonSpeciesData from './data/pokemonSpecies.js'
-import languagesData from './data/pokemonLanguages'
+import POKEMON_DATA from './data/data'
 
 class App extends React.Component {
   state = {
-    pokemonSpecies: pokemonSpeciesData,
-    pokemonEnglishNames: [],
-    languages: languagesData,
     inputValue: '',
+    pokemonData: POKEMON_DATA
   }
 
   handleChange = (e) => {
@@ -22,36 +19,6 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    const pokemonActual = [];
-
-    for(let i = 807; i != 0; i--) {
-      pokemonActual.push({});
-    }
-
-    let counter = 1;
-    let lang = "";
-
-    this.state.pokemonSpecies.forEach(element => {
-      if(element.pokemon_species_id === counter) {
-        this.state.languages.forEach((e) => {
-          if(element.local_language_id === e.id) {
-            pokemonActual[counter][e.iso3166] = element.name
-          }
-        })
-      }
-      else {
-        pokemonActual[counter].id = element.pokemon_species_id
-        pokemonActual[counter].sprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${counter}.png`
-        counter = counter + 1
-        this.state.languages.forEach((e) => {
-          if(element.local_language_id === e.id) {
-            pokemonActual[counter][e.iso3166] = element.name
-          }
-        })
-      }
-      console.log(pokemonActual)
-    })
-    
   }
 
  render() {
