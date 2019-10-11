8c49866ba3428095b48f5a4cd0774f6f750e0dd3
Mortzcent <moryckuba@gmail.com>
2019-10-11 16:51:52 +0200

    Updates

---
+++ precache-manifest.f5b814235a4c220233a7783c4ad59b44.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77eff2e289f038f4bfb9ca8a7ad262e",
+    "url": "/pokedex-languages/./index.html"
+  },
+  {
+    "revision": "8cfa8af1783658c9bf31",
+    "url": "/pokedex-languages/./static/css/2.023e65de.chunk.css"
+  },
+  {
+    "revision": "408994f030bc7cb73ead",
+    "url": "/pokedex-languages/./static/css/main.4f871438.chunk.css"
+  },
+  {
+    "revision": "8cfa8af1783658c9bf31",
+    "url": "/pokedex-languages/./static/js/2.13b8326f.chunk.js"
+  },
+  {
+    "revision": "408994f030bc7cb73ead",
+    "url": "/pokedex-languages/./static/js/main.97e60db9.chunk.js"
+  },
+  {
+    "revision": "5f22d719cc2039534b58",
+    "url": "/pokedex-languages/./static/js/runtime~main.8bf37ea8.js"
+  },
+  {
+    "revision": "7f8455b70734a87596991e1f24053edc",
+    "url": "/pokedex-languages/./static/media/china.7f8455b7.svg"
+  },
+  {
+    "revision": "968aaa24eeffdd779e4fc9c27c5cb730",
+    "url": "/pokedex-languages/./static/media/france.968aaa24.svg"
+  },
+  {
+    "revision": "d810f62137d64a0a58035c80489b7d54",
+    "url": "/pokedex-languages/./static/media/germany.d810f621.svg"
+  },
+  {
+    "revision": "f0b18641a0f32d33cfa285dfd1168738",
+    "url": "/pokedex-languages/./static/media/icons8-upward.f0b18641.svg"
+  },
+  {
+    "revision": "612e617f5d72975e63e53d793d757785",
+    "url": "/pokedex-languages/./static/media/italy.612e617f.svg"
+  },
+  {
+    "revision": "1f905d23af144bc76e488f00d516de2e",
+    "url": "/pokedex-languages/./static/media/japan.1f905d23.svg"
+  },
+  {
+    "revision": "8a067f19c6a32f781b8cb971a3214629",
+    "url": "/pokedex-languages/./static/media/south-korea.8a067f19.svg"
+  },
+  {
+    "revision": "36938bbe277961386e7e309dcb30cbd8",
+    "url": "/pokedex-languages/./static/media/spain.36938bbe.svg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/pokedex-languages/./static/media/united-kingdom.e5564902.svg"
+  }
+]);
